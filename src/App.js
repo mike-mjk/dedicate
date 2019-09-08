8445fee814493925e7ac6ea3b3d17c40f42438de
mike-mjk <michael.kostolnik@gmail.com>
2019-09-08 11:15:09 -0700

    render dedication list

--- src/App.js
+++ src/App.js
@@ -3,34 +3,13 @@ import "./App.css";
 import CreateDedication from "./CreateDedication";
 import DedicationList from "./DedicationList";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      dedications: [{ message: "", name: "" }]
-    };
-  }
-
-  componentDidMount() {
-    fetch("/api/get_dedications")
-      .then(response => {
-        return response.json();
-      })
-      .then(json => {
-        console.log(json);
-        this.setState({ dedications: json });
-      });
-  }
-  render() {
-    const { message, name } = this.state.dedications[0];
-    return (
-      <React.Fragment>
-        <CreateDedication />
-        <DedicationList />
-      </React.Fragment>
-    );
-  }
+function App() {
+  return (
+    <React.Fragment>
+      <CreateDedication />
+      <DedicationList />
+    </React.Fragment>
+  );
 }
 
 export default App;
--- src/DedicationList.js
+++ src/DedicationList.js
@@ -1,14 +1,35 @@
 import React from "react";
+import Dedication from "./Dedication";
 
 class DedicationList extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = [];
+    this.state = {
+      dedications: [{ message: "", name: "" }]
+    };
+  }
+
+  componentDidMount() {
+    fetch("/api/get_dedications")
+      .then(response => {
+        return response.json();
+      })
+      .then(json => {
+        console.log(json);
+        this.setState({ dedications: json });
+      });
+  }
+
+  renderDedications() {
+    return this.state.dedications.map(dedication => {
+      const { name, message } = dedication;
+      return <Dedication name={name} message={message} />;
+    });
   }
 
   render() {
-    return <div> DedicationList</div>;
+    return <div>{this.renderDedications()}</div>;
   }
 }
 
