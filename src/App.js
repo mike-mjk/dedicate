5db1ede721c0736471ba5ed5f3fa54f07cf0da0c
mike-mjk <michael.kostolnik@gmail.com>
2019-10-11 19:49:48 -0700

    simplest heading

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import DedicationList from "./DedicationList";
 function App() {
   return (
     <React.Fragment>
+      <h1>Formally dedicate your time to a friend or loved one</h1>
       <CreateDedication />
       <DedicationList />
     </React.Fragment>
